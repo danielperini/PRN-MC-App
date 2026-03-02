29c9e2862fdf93f9eea25ec32df28b83153e5e7e
Daniel Perini <daniel@periniprojetos.com.br>
2026-03-02 19:04:10 +0000

    File changes

--- src/pages/CoordReview.jsx
+++ src/pages/CoordReview.jsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { base44 } from '@/api/base44Client';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import RequireAuth from '../components/auth/RequireAuth';
+import { useCurrentUser } from '../components/auth/useCurrentUser';
 import { Link, useNavigate } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { 
--- src/pages/Relatorios.jsx
+++ src/pages/Relatorios.jsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { base44 } from '@/api/base44Client';
 import { useQuery } from '@tanstack/react-query';
+import RequireAuth from '../components/auth/RequireAuth';
+import { useCurrentUser } from '../components/auth/useCurrentUser';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import {
