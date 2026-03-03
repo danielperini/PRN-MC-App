f84c0f21b1b5039ea30e31eb880bbe614da715b9
Daniel Perini <daniel@periniprojetos.com.br>
2026-03-03 05:43:33 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -109,7 +109,7 @@ function AuthenticatedHome({ user }) {
   ];
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-white">
       {/* Top bar */}
       <header className="bg-white border-b border-gray-100 sticky top-0 z-10">
         <div className="max-w-6xl mx-auto px-6 py-4 flex items-center justify-between">
