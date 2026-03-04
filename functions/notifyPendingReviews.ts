2d8309cb2bcdb237c15dab09edd318b68e9ecc48
Daniel Perini <daniel@periniprojetos.com.br>
2026-03-04 15:11:11 +0000

    File changes

--- functions/notifyPendingReviews.ts
+++ functions/notifyPendingReviews.ts
@@ -3,11 +3,8 @@ import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 Deno.serve(async (req) => {
   try {
     const base44 = createClientFromRequest(req);
-    const user = await base44.auth.me();
-
-    if (!user || user.role !== 'admin') {
-      return Response.json({ error: 'Forbidden' }, { status: 403 });
-    }
+    
+    // Automação executada como serviço, não necessita autenticação de usuário
 
     // Get all coordinators
     const coordinators = await base44.asServiceRole.entities.User.filter(
