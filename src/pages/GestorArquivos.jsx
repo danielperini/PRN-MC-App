07f1aff764f7729b59a6ca2b3f6f4940e28a0a43
Daniel Perini <daniel@periniprojetos.com.br>
2026-03-04 20:39:10 +0000

    File changes

--- src/pages/GestorArquivos.jsx
+++ src/pages/GestorArquivos.jsx
@@ -9,7 +9,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import BackupButton from '../components/backup/BackupButton';
-import FilePreview from '../components/gallery/FilePreview';
+import FileHierarchy from '../components/gallery/FileHierarchy';
 import { toast } from 'sonner';
 
 function GestorArquivosInner() {
