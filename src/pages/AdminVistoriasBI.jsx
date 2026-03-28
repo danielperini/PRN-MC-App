f2f1f7f49757c5b21c353b0da96b64fe2263ea2f
danielperini <32501391+danielperini@users.noreply.github.com>
2026-03-28 02:36:57 -0300

    Update AdminVistoriasBI.jsx

--- src/pages/AdminVistoriasBI.jsx
+++ src/pages/AdminVistoriasBI.jsx
@@ -1,1 +1,9 @@
+import React from "react";
 
+export default function AdminVistoria() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-semibold">Admin Vistoria</h1>
+    </div>
+  );
+}
