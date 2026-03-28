0b1c10ca27a0c01009f0e1a89e444fd048c2cde5
danielperini <32501391+danielperini@users.noreply.github.com>
2026-03-28 02:37:44 -0300

    Update PaginaNaoEncontrada.jsx

--- src/pages/PaginaNaoEncontrada.jsx
+++ src/pages/PaginaNaoEncontrada.jsx
@@ -1,1 +1,9 @@
+import React from "react";
 
+export default function PaginaNaoEncontrada() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-semibold">Página não encontrada</h1>
+    </div>
+  );
+}
