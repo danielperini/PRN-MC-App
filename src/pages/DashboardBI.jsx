1088aa59336e8abf3e5325a39e009bfda8948560
danielperini <32501391+danielperini@users.noreply.github.com>
2026-03-28 02:34:18 -0300

    Update DashboardBI.jsx

--- src/pages/DashboardBI.jsx
+++ src/pages/DashboardBI.jsx
@@ -1,1 +1,9 @@
+import React from "react";
 
+export default function DashboardBI() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-semibold">Dashboard BI</h1>
+    </div>
+  );
+}
