19c60a513ea425c54bdd9f52368800290bcbab43
danielperini <32501391+danielperini@users.noreply.github.com>
2026-03-29 15:12:50 -0300

    Update useSyncProgramacao.js

--- src/hooks/useSyncProgramacao.js
+++ src/hooks/useSyncProgramacao.js
@@ -1,10 +1,16 @@
-export async function syncProgramacao() {
-  try {
-    const res = await base44.functions.syncProgramacao();
-    console.log("SYNC OK:", res);
-    return res;
-  } catch (error) {
-    console.error("SYNC ERROR:", error);
-    throw error;
-  }
+import { base44 } from '@/api/base44Client';
+
+const PROGRAMACAO_SOURCE_URL =
+  'https://docs.google.com/spreadsheets/d/1I8Tbj5URR7gEX_zZEAFVIkAAfBCs58LC/edit?usp=drive_link&ouid=114388859796899599894&rtpof=true&sd=true';
+
+export async function syncProgramacao(extraArgs = {}) {
+  const response = await base44.functions.invoke('syncProgramacao', {
+    source_url: PROGRAMACAO_SOURCE_URL,
+    title: 'Programação espelhada',
+    mode: 'history',
+    debug: '1',
+    ...extraArgs,
+  });
+
+  return response?.data || response;
 }
