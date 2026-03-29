f9ba28a2f6a6d2797199077f487379d86ea738ca
danielperini <32501391+danielperini@users.noreply.github.com>
2026-03-29 15:34:44 -0300

    Update useSyncProgramacao.js

--- src/hooks/useSyncProgramacao.js
+++ src/hooks/useSyncProgramacao.js
@@ -1,14 +1,10 @@
 import { base44 } from '@/api/base44Client';
 
-const PROGRAMACAO_SOURCE_URL =
-  'https://docs.google.com/spreadsheets/d/1I8Tbj5URR7gEX_zZEAFVIkAAfBCs58LC/edit?usp=drive_link&ouid=114388859796899599894&rtpof=true&sd=true';
+const PROGRAMACAO_FILE_NAME = 'Planilha_de_programação_MC-VAR (1).xlsx';
 
 export async function syncProgramacao(extraArgs = {}) {
   const response = await base44.functions.invoke('syncProgramacao', {
-    source_url: PROGRAMACAO_SOURCE_URL,
-    title: 'Programação espelhada',
-    mode: 'history',
-    debug: '1',
+    file_name: PROGRAMACAO_FILE_NAME,
     ...extraArgs,
   });
 
