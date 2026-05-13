bf794a0bc8fc1ddaee21de74b2db44d19a59aae6
danielperini <32501391+danielperini@users.noreply.github.com>
2026-05-12 21:58:46 -0300

    feat: ativar sincronizacao automatica de backup drive

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import DashboardPatrocinador from './pages/DashboardPatrocinadorSync';
 import EntradaUnica from './pages/EntradaUnica.jsx';
 import Mensagens from './pages/Mensagens.jsx';
 import GuiaNotaFiscal from './pages/GuiaNotaFiscal';
+import NFDriveBackupSyncInstaller from '@/lib/nfDriveBackupSync';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
@@ -150,43 +151,47 @@ function AuthenticatedApp() {
   }
 
   return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        key={location.pathname}
-        initial={{ opacity: 0, x: 10 }}
-        animate={{ opacity: 1, x: 0 }}
-        exit={{ opacity: 0, x: -10 }}
-        transition={{ duration: 0.25, ease: 'easeInOut' }}
-      >
-        <Routes>
-          <Route
-            path="/"
-            element={<SafePage Page={MainPage} pageName={mainPageKey} />}
-          />
+    <>
+      <NFDriveBackupSyncInstaller />
 
-          {Object.entries(Pages).map(([path, Page]) => (
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={location.pathname}
+          initial={{ opacity: 0, x: 10 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -10 }}
+          transition={{ duration: 0.25, ease: 'easeInOut' }}
+        >
+          <Routes>
             <Route
-              key={path}
-              path={`/${path}`}
-              element={<SafePage Page={Page} pageName={path} />}
+              path="/"
+              element={<SafePage Page={MainPage} pageName={mainPageKey} />}
             />
-          ))}
 
-          <Route path="/ChecklistProducao" element={<SafePage Page={ChecklistProducao} pageName="ChecklistProducao" />} />
-          <Route path="/RubricasPorMuseu" element={<SafePage Page={RubricasPorMuseu} pageName="RubricasPorMuseu" />} />
-          <Route path="/BaseConhecimento" element={<SafePage Page={BaseConhecimento} pageName="BaseConhecimento" />} />
-          <Route path="/ProgramacaoEspelho" element={<SafePage Page={ProgramacaoEspelho} pageName="ProgramacaoEspelho" />} />
-          <Route path="/Agenda" element={<SafePage Page={Agenda} pageName="Agenda" />} />
-          <Route path="/DashboardPatrocinador" element={<SafePage Page={DashboardPatrocinador} pageName="DashboardPatrocinador" />} />
-          <Route path="/EntradaUnica" element={<SafePage Page={EntradaUnica} pageName="EntradaUnica" />} />
-          <Route path="/Mensagens" element={<SafePage Page={Mensagens} pageName="Mensagens" />} />
-          <Route path="/GuiaNotaFiscal" element={<SafePage Page={GuiaNotaFiscal} pageName="GuiaNotaFiscal" />} />
-          <Route path="/Aparencia" element={<SafePage Page={Aparencia} pageName="Aparencia" />} />
+            {Object.entries(Pages).map(([path, Page]) => (
+              <Route
+                key={path}
+                path={`/${path}`}
+                element={<SafePage Page={Page} pageName={path} />}
+              />
+            ))}
 
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </motion.div>
-    </AnimatePresence>
+            <Route path="/ChecklistProducao" element={<SafePage Page={ChecklistProducao} pageName="ChecklistProducao" />} />
+            <Route path="/RubricasPorMuseu" element={<SafePage Page={RubricasPorMuseu} pageName="RubricasPorMuseu" />} />
+            <Route path="/BaseConhecimento" element={<SafePage Page={BaseConhecimento} pageName="BaseConhecimento" />} />
+            <Route path="/ProgramacaoEspelho" element={<SafePage Page={ProgramacaoEspelho} pageName="ProgramacaoEspelho" />} />
+            <Route path="/Agenda" element={<SafePage Page={Agenda} pageName="Agenda" />} />
+            <Route path="/DashboardPatrocinador" element={<SafePage Page={DashboardPatrocinador} pageName="DashboardPatrocinador" />} />
+            <Route path="/EntradaUnica" element={<SafePage Page={EntradaUnica} pageName="EntradaUnica" />} />
+            <Route path="/Mensagens" element={<SafePage Page={Mensagens} pageName="Mensagens" />} />
+            <Route path="/GuiaNotaFiscal" element={<SafePage Page={GuiaNotaFiscal} pageName="GuiaNotaFiscal" />} />
+            <Route path="/Aparencia" element={<SafePage Page={Aparencia} pageName="Aparencia" />} />
+
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </motion.div>
+      </AnimatePresence>
+    </>
   );
 }
 
