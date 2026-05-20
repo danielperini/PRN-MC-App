1d82181086e0ff57e85a0c6c85528baf43b9311b
danielperini <32501391+danielperini@users.noreply.github.com>
2026-05-20 20:06:13 -0300

    Ativa correções runtime do relatório

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from '@/App.jsx'
 import '@/index.css'
 import '@/styles/theme-nuit.css'
 import '@/styles/report-print-fixes.css'
+import '@/utils/reportHtmlRuntimeFixes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
