9cd749f4cf34a03e4ba2e78830eab94584a61d9f
danielperini <32501391+danielperini@users.noreply.github.com>
2026-05-20 22:00:43 -0300

    Carrega correção hard do relatório PDF

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import '@/index.css'
 import '@/styles/theme-nuit.css'
 import '@/styles/report-print-fixes.css'
 import '@/utils/safeIndexedDbPreviewStorage.js'
+import '@/utils/reportPdfHardLayoutFix'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
