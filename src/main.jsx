de5c6c8ca30dbc8cbfa1e83cde5df3a44151819a
danielperini <32501391+danielperini@users.noreply.github.com>
2026-07-15 17:55:02 -0300

    Ativar PDF com galeria final por atividade

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,10 @@ import App from '@/App.jsx'
 import '@/index.css'
 import '@/styles/theme-nuit.css'
 import '@/styles/report-print-fixes.css'
+import { installActivityGalleryPdfRouting } from '@/utils/installActivityGalleryPdfRouting'
 import { installPdfDownloadGuard } from '@/utils/pdfDownloadGuard'
 
+installActivityGalleryPdfRouting()
 installPdfDownloadGuard()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
