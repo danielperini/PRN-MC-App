965ec9eaba436d209f2bc1e529c0bf4510439f75
danielperini <32501391+danielperini@users.noreply.github.com>
2026-07-16 13:23:30 -0300

    Ativar coluna de metas na tabela de Compras

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,12 @@ import '@/styles/theme-nuit.css'
 import '@/styles/report-print-fixes.css'
 import { installActivityGalleryPdfRouting } from '@/utils/installActivityGalleryPdfRouting'
 import { installComprasDataNFFilter } from '@/utils/installComprasDataNFFilter'
+import { installComprasMetaColumn } from '@/utils/installComprasMetaColumn'
 import { installPdfDownloadGuard } from '@/utils/pdfDownloadGuard'
 
 installActivityGalleryPdfRouting()
 installComprasDataNFFilter()
+installComprasMetaColumn()
 installPdfDownloadGuard()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
