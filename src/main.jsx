fc3b6ead5dc2f9f6a715777c9e8f3cc67ac7b70a
danielperini <32501391+danielperini@users.noreply.github.com>
2026-07-16 11:15:24 -0300

    Ativar filtro de Compras pela data de emissão da NF

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import '@/index.css'
 import '@/styles/theme-nuit.css'
 import '@/styles/report-print-fixes.css'
 import { installActivityGalleryPdfRouting } from '@/utils/installActivityGalleryPdfRouting'
+import { installComprasDataNFFilter } from '@/utils/installComprasDataNFFilter'
 import { installPdfDownloadGuard } from '@/utils/pdfDownloadGuard'
 
 installActivityGalleryPdfRouting()
+installComprasDataNFFilter()
 installPdfDownloadGuard()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
